78006964c7ebcf1369af6a6a2678473bbad5deaa
hugohua <baofen14787@gmail.com>
2016-12-21 23:02:58 +0800

    完成基础版本

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
+
 /**
- * Created by hugo on 16/12/6.
+ * Created by hugo on 16/12/18.
+ * 获取商品详情内容
  */
-"use strict";
 
-const Crawler = require("crawler");
-const desktop = require('./data/desktop.json');
+'use strict';
 
-let desktopData = [];
 
-var c = new Crawler({
-	maxConnections : 10,
-	// This will be called for each crawled page
-	callback : function (error, result, $) {
-		// $ is Cheerio by default
-		//a lean implementation of core jQuery designed specifically for the server
-		if(error){
-			console.log(error);
-		}else{
-			console.log($("h1").text());
-		}
-	}
+const co = require('co');
+const path = require('path');
+const log = require('fie-log')('spider');
+const Excel = require('./lib/excel');
+const details = require('./lib/details');
+const config = require('./lib/config');
+const urls = require('./data/urls.json');
+
+co(function* () {
+  const excel = new Excel();
+
+  const tableHeader = [
+		{ header: '商品', key: 'title', width : 100 },
+		{ header: '序号', key: 'buyingNum', width : 15 },
+		{ header: '采购单位', key: 'buyingUnit', width : 20 },
+		{ header: '数量', key: 'buyQuantity', width : 15 },
+		{ header: '单价', key: 'buyingPrice', width : 15 },
+		{ header: '电商', key: 'buyingUser', width : 20 },
+		{ header: '采购时间', key: 'buyingDate', width : 15 }
+  ];
+
+	//TODO 抓取列表页
+  const data = yield details.get([
+    'http://112.74.98.194/commodities/15367?p_id=16150',
+    'http://112.74.98.194/commodities/24285?p_id=25617'
+  ]);
+
+  excel.addWorksheet('台式机', tableHeader, data);
+  excel.save('data/file2.xlsx', () => {
+  });
+}).catch((e) => {
+  console.log(e);
 });
-
-c.queue(desktop.urls);
--- test/excel-spec.js
+++ test/excel-spec.js
@@ -1,31 +1,29 @@
-"use strict";
+'use strict';
 
 const Excel = require('../lib/excel');
 
 describe('excel', () => {
-
-	it('保存文件', (done)=> {
-		const excel = new Excel();
-		const header = // 品牌	商品	类型	处理器	内存	硬盘	显卡	显示器	系统	单价	电商平台	采购单位	采购数量	采购时间
-			[
-				{header:'品牌',key:'brand'},
-				{header:'商品',key:'title'},
-				{header:'类型',key:'type'},
-				{header:'处理器',key:'cpu'},
-				{header:'内存',key:'memory'},
-				{header:'硬盘',key:'disk'},
-				{header:'显卡',key:'gpu'},
-				{header:'显示器',key:'display'},
-				{header:'系统',key:'system'},
-				{header:'单价',key:'price'},
-				{header:'电商平台',key:'platform'},
-				{header:'采购数量',key:'number'},
-				{header:'采购时间',key:'time'}
-			];
-		excel.addWorksheet('台式机',header,[]);
-		excel.save( 'data/file.xlsx', () => {
-			done();
-		});
-	})
-
+  it('保存文件', (done) => {
+    const excel = new Excel();
+    const header = // 品牌	商品	类型	处理器	内存	硬盘	显卡	显示器	系统	单价	电商平台	采购单位	采购数量	采购时间
+      [
+				{ header: '品牌', key: 'brand' },
+				{ header: '商品', key: 'title' },
+				{ header: '类型', key: 'type' },
+				{ header: '处理器', key: 'cpu' },
+				{ header: '内存', key: 'memory' },
+				{ header: '硬盘', key: 'disk' },
+				{ header: '显卡', key: 'gpu' },
+				{ header: '显示器', key: 'display' },
+				{ header: '系统', key: 'system' },
+				{ header: '单价', key: 'price' },
+				{ header: '电商平台', key: 'platform' },
+				{ header: '采购数量', key: 'number' },
+				{ header: '采购时间', key: 'time' }
+      ];
+    excel.addWorksheet('台式机', header, []);
+    excel.save('data/file.xlsx', () => {
+      done();
+    });
+  });
 });
