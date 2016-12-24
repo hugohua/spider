5293a1c74741925af78051dae4735389aef98f06
hugohua <baofen14787@gmail.com>
2016-12-24 14:27:11 +0800

    完善台式机型号

--- index.js
+++ index.js
@@ -12,42 +12,91 @@ const path = require('path');
 const log = require('fie-log')('spider');
 const Excel = require('./lib/excel');
 const details = require('./lib/details');
+const Desktop = require('./lib/desktop');
 const config = require('./lib/config');
 const urls = require('./data/urls.json');
+const fs = require('co-fs-extra');
+/**
+ * 导出通用类目
+ */
+function* exportOther(){
+  return new Promise( (resolve, reject) => {
+    const excel = new Excel();
+
+    const tableHeader = [
+      { header: '商品', key: 'title', width : 100 },
+      { header: '电商平台', key: 'platform', width : 20 },
+      { header: '序号', key: 'buyingNum', width : 15 },
+      { header: '采购单位', key: 'buyingUnit', width : 20 },
+      { header: '数量', key: 'buyQuantity', width : 15 },
+      { header: '单价', key: 'buyingPrice', width : 15 },
+      { header: '累计采购金额', key: 'totalPrice', width : 15 },
+      { header: '电商', key: 'buyingUser', width : 20 },
+      { header: '采购时间', key: 'buyingDate', width : 15 },
+      { header: 'URL', key: 'url', width : 60 }
+
+    ];
+
+    //TODO 抓取列表页
+    //const data = yield details.get([
+    //  'http://112.74.98.194/commodities/15367?p_id=16150',
+    //  'http://112.74.98.194/commodities/24285?p_id=25617'
+    //]);
+
+    //const data = yield details.get(urls['117']);
+
+    //for(let i in urls){
+    //  let cate = config.cate[i];
+    //  log.info(`--> 开始抓取 ${cate} 类目....`);
+    //  const data = yield details.get(urls[i]);
+    //  excel.addWorksheet(cate, tableHeader, data);
+    //  log.success(`--> 成功抓取 ${cate} 类目下的${data.length}条数据....`)
+    //}
+
+    excel.addWorksheet('台式机', tableHeader, data);
+    excel.save('data/file2.xlsx', () => {
+      resolve();
+    });
+  })
+}
+
+
 
 co(function* () {
   const excel = new Excel();
 
   const tableHeader = [
-		{ header: '商品', key: 'title', width : 100 },
+    { header: '商品', key: 'title', width : 100 },
+    { header: '品牌', key: 'brand', width : 15 },
+    { header: '型号', key: 'type', width : 15 },
+    { header: '处理器', key: 'cpu', width : 15 },
+    { header: '显卡', key: 'gpu', width : 15 },
+    { header: '内存', key: 'memory', width : 15 },
+    { header: '硬盘大小', key: 'disk', width : 15 },
+    { header: '操作系统', key: 'system', width : 15 },
+    { header: '显示器', key: 'display', width : 15 },
     { header: '电商平台', key: 'platform', width : 20 },
-		{ header: '序号', key: 'buyingNum', width : 15 },
-		{ header: '采购单位', key: 'buyingUnit', width : 20 },
-		{ header: '数量', key: 'buyQuantity', width : 15 },
-		{ header: '单价', key: 'buyingPrice', width : 15 },
-		{ header: '电商', key: 'buyingUser', width : 20 },
-		{ header: '采购时间', key: 'buyingDate', width : 15 },
+    { header: '序号', key: 'buyingNum', width : 15 },
+    { header: '采购单位', key: 'buyingUnit', width : 20 },
+    { header: '数量', key: 'buyQuantity', width : 15 },
+    { header: '单价', key: 'buyingPrice', width : 15 },
+    { header: '累计采购金额', key: 'totalPrice', width : 15 },
+    { header: '电商', key: 'buyingUser', width : 20 },
+    { header: '采购时间', key: 'buyingDate', width : 15 },
     { header: 'URL', key: 'url', width : 60 }
 
   ];
 
-	//TODO 抓取列表页
-  const data = yield details.get([
-    'http://112.74.98.194/commodities/15367?p_id=16150',
-    'http://112.74.98.194/commodities/24285?p_id=25617'
-  ]);
+  const data = yield details.get(urls['177']);
 
-  //for(let i in urls){
-  //  let cate = config.cate[i];
-  //  log.info(`--> 开始抓取 ${cate} 类目....`);
-  //  const data = yield details.get(urls[i]);
-  //  excel.addWorksheet(cate, tableHeader, data);
-  //  log.success(`--> 成功抓取 ${cate} 类目下的${data.length}条数据....`)
-  //}
+  const file = path.join(__dirname, '../data/177.json');
+  // 写入project文件
+  yield fs.outputJson(file, data);
 
-  excel.addWorksheet('台式机', tableHeader, data);
-  excel.save('data/file2.xlsx', () => {
-  });
+  log.success(`成功将数据写入: ${file}`);
+
+
+
 }).catch((e) => {
   console.log(e);
 });
--- test/desktop-spec.js
+++ test/desktop-spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Desktop = require('../lib/desktop');
+const cateData = require('../data/177.json');
 
 describe('rules', () => {
   it(' 戴尔台式标题替换 ', () => {
@@ -37,6 +38,40 @@ describe('rules', () => {
     });
   });
 
+  it(' 联想G系列处理器 ', () => {
+    const data = new Desktop('联想 Lenovo 台式电脑套机 启天M4650-B003 19.5寸 G3900 4G 1T 集显 DVDRW 无系统 三年上门');
+
+    expect(data).to.be.an('object');
+
+    expect(data).to.deep.equal({
+      brand: '联想',
+      type: '启天M4650-B003',
+      cpu: 'g3900',
+      gpu: '集显',
+      memory: '4G',
+      disk: '1T',
+      system: 'DOS',
+      display: '19.5寸'
+    });
+  });
+
+  it(' 联想M系列处理器 ', () => {
+    const data = new Desktop('联想（Lenovo）ThinkCentreM8600t-D001台式电脑I5-6500/4G/500G/DVDRW/集成显卡/Win7Pro五年上门+21.5英寸');
+
+    expect(data).to.be.an('object');
+
+    expect(data).to.deep.equal({
+      brand: '联想',
+      type: 'M8600t-D001',
+      cpu: 'i5-6500',
+      gpu: '集成显卡',
+      memory: '4G',
+      disk: '500G',
+      system: 'Win7Pro五年上门',
+      display: '21.5英寸'
+    });
+  });
+
 
   it(' 联想带括号 ', () => {
     const data = new Desktop('联想（Lenovo）启天M4600-B032台式电脑（I3-6100/H110/4G/500G/DVDRW/集成显卡/DOS)五年上门保修+19.5宽屏');
@@ -54,4 +89,24 @@ describe('rules', () => {
       display: '19.5宽屏'
     });
   });
+
+  it.only( '全类目处理测试', () => {
+    cateData.forEach(item => {
+      console.log(item.title);
+      console.log(item.url);
+
+      let data = new Desktop(item.title);
+      expect(data).to.be.an('object');
+      //expect(data.brand).to.not.equal('');
+      //expect(data.type).to.not.equal('');
+      //expect(data.cpu).to.not.equal('');
+      //expect(data.gpu).to.not.equal('');
+      //expect(data.memory).to.not.equal('');
+      //expect(data.disk).to.not.equal('');
+      //expect(data.system).to.not.equal('');
+      //expect(data.display).to.not.equal('');
+
+    })
+  } )
+
 });
